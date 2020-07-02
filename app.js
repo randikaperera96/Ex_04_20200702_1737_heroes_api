7a9bad76a93fe2e90d15fa07e6c9c4f1d220254f
randikaperera96 <randikaperera135@gmail.com>
2020-07-02 19:31:14 +0530

    Demo middleware basics

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const { json } = require('express');
 const app = express();// top level function of express [ similar to "costructor" in OOP ]
 const PORT = 5000;
 
-app.use(express.json()); //to read POST or PUT request body as JSON 
+app.use(express.json()); //to read POST or PUT request body as JSON
+
+app.use((req, res, next)=>{
+    console.log('1st middleware');
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log('2nd middleware');
+    next();
+});
 
 let heroArrray = [
     {
